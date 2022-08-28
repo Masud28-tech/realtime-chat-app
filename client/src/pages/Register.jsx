bc4fc591ca8956a29be72333224996b83340ae53
Masud28-Tech <masud.tech28@gmail.com>
2022-08-28 15:23:10 +0530

    Commit 1: worked on 'Register' page with UI.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '../assets/chat-logo.svg';
 
+import { ToastContainer, toast } from 'react-toastify'; // For warning
+import 'react-toastify/dist/ReactToastify.css';
+
+const defaultValues = {
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
 const Register = () => {
+
+  const [credentials, setCredentials] = useState(defaultValues);
+  const { username, email, password, confirmPassword } = credentials;
+
+  const handleChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert('Form submitted');
+    handleValidation();
   }
-  const handleChange = (e) => {
 
+  const handleValidation = () => {
+    const { password, confirmPassword } = credentials;
+    if (password !== confirmPassword) {
+      toast.error("Password and Confirm Password are not same!");
+      return false;
+    }
+    else if (password.length < 5) {
+      toast.error("Password must have atleast 5 characters");
+      return false;
+    }
+    else if (username.length < 1) {
+      toast.error("Username must have atleast 1 character");
+      return false;
+    }
+    else if (!email.length) {
+      toast.error("email must not empty");
+      return false;
+    }
+    return true;
   }
+
+
   return (
     <>
       <FormContainer>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div className="brand">
             <img src={Logo} alt="logo" />
             <h1>Hommies Chat</h1>
@@ -22,41 +59,117 @@ const Register = () => {
 
           <input
             type="text"
-            placeholder='Name'
-            name='name'
-            onClick={handleChange}
-          />
-          <input
-            type="text"
-            placeholder='username'
             name='username'
-            onClick={handleChange}
+            value={username}
+            placeholder='User Name'
+            onChange={(e) => handleChange(e)}
           />
           <input
-            type="password"
-            placeholder='Password'
+            type="email"
+            name='email'
+            value={email}
+            placeholder='Email'
+            onChange={(e) => handleChange(e)}
+          />
+          <input
             name='password'
-            onClick={handleChange}
+            type="password"
+            value={password}
+            placeholder='Password'
+            onChange={(e) => handleChange(e)}
           />
           <input
             type="password"
-            placeholder='Confirm Password'
             name='confirmPassword'
-            onClick={handleChange}
+            value={confirmPassword}
+            placeholder='Confirm Password'
+            onChange={(e) => handleChange(e)}
           />
 
           <button type='submit'>Create User</button>
 
-          <span> Already have an account? <Link to='/login'>Login</Link></span>
+          <span> Already have an account ?  <Link to='/login'> Login </Link></span>
 
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   )
 }
 
 const FormContainer = styled.div`
-  
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background-color:#2D4059;
+
+  .brand{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img{
+      height: 5rem;
+    }
+    h1{
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form{
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #FF5722;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input{
+      /* background-color: transparent; */
+      padding: 1rem;
+      border: 0.2rem solid #222831;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      width: 100%;
+      &:focus{
+        border: 0.2rem solid #F8CB2E;
+        outline: none;
+      }
+    }
+
+    button{
+      padding: 1rem 2rem;
+      background:#035397;
+      color: white;
+      border: none;
+      border-radius: 0.4rem;
+      font-size:1rem;
+      font-weight: bold;
+      text-transform:uppercase;
+      cursor:pointer;
+      transition: 0.5s ease-in-out;
+      &:hover{
+        background-color:#001E6C;
+      }
+    }
+
+    span{
+      color: white;
+      text-transform: uppercase;
+      a{
+        color:#2155CD;
+        font-weight: bold;
+        text-decoration: none;
+      }
+    }
+
+  }
+
+
 `
 
 export default Register;
