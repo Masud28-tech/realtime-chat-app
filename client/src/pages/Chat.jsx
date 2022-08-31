558489c81735a923316e0a6d15b3a0ef4513b40d
Masud28-Tech <masud.tech28@gmail.com>
2022-08-31 14:38:48 +0530

    Commit 3: Work on chat page: contacts component is done with backend and UI features such as, getAllUser routes and display all users and current user onn chat page.

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
+
+import { getAllUsersRoute } from '../utils/APIRoutes';
+import Contacts from '../components/Contacts';
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
-}
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
 
-export default Chat
+  useEffect(() => {
+    async function checkForUser() {
+      if (!localStorage.getItem('chat-app-user')) {
+        navigate('/login');
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')));
+      }
+    };
+    checkForUser();
+  }, []);
+
+  useEffect(() => {
+    async function fetchContactsFromCurrentUser() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${getAllUsersRoute}/${currentUser._id}`);
+          setContacts(data);
+        } else {
+          navigate('/setAvatar');
+        }
+      }
+    };
+    fetchContactsFromCurrentUser();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  }
+
+  return (
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts.data} currentUser={currentUser} chatChange={handleChatChange} />
+      </div>
+    </Container>
+  )
+};
+
+const Container = styled.div`
+  height:100vh;
+  width:100vw;
+  background-color:#2D4059;
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items: center;
+  gap:1rem;
+
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color:#112B3C;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px){
+      grid-template-columns: 35% 65%;
+    }
+  }
+`
+
+export default Chat;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -77,4 +77,20 @@ module.exports.setAvatar = async (req, res, next) => {
     } catch (ex) {
         next(ex);
     }
-}
+};
+
+module.exports.getAllUsers = async (req, res, next)=> {
+    try{
+        const users = await User.find({_id: {$ne: req.params.id}}).select([
+            'email',
+            'username',
+            'avatarImage',
+            '_id'
+        ]);
+
+        return res.json(users);
+    }
+    catch(ex){
+        next(ex);
+    }
+};
